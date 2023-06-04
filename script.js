973992089a3c661f1ef2df9a151c1ac41dc0c834
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-06-04 15:35:49 -0400

    Ajuste de navbar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Función que me aplica el estilo a la opción seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
     var opciones = document.querySelectorAll('#links  a');
+    // opciones[0].className = "";
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
-    opciones[4].className = "";
     link.className = "seleccionado";
 
     //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
